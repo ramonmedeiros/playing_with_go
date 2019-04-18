2e14dbfa05f1a2bcfa784abf519d6593d261ab03
Ramon Medeiros <ramon.rnm@gmail.com>
2019-04-18 10:20:21 +0200

    clean up

--- src/main/hello.go
+++ src/main/hello.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-//    "flag"
+    "flag"
 	"fmt"
 	"strings"
 	"net/http"
@@ -9,70 +9,52 @@ import (
     "os"
     "github.com/gin-gonic/gin"
     "encoding/json"
+    "time"
 )
 
-import "github.com/buger/jsonparser"
 const ABIOS_URL = "https://api.abiosgaming.com/v2"
-const CLIENT_ID = "test-task"
-
-type listOfJson struct {
-    List []interface{}
-}
 
 var atoken string
 
 func main() {
-    //client_id := flag.String("id", "", "client id for Abios API")
-    //client_key := flag.String("key", "", "client key for Abios API")
-    //flag.Parse()
+    client_id := flag.String("id", "", "client id for Abios API")
+    client_key := flag.String("key", "", "client key for Abios API")
+    flag.Parse()
 
 	server := gin.Default()
 
-    /*var err error
-    atoken, err = getAccessToken(*client_key, *client_id)
+    atoken = getAccessToken(*client_key, *client_id)
 
-    if err != nil {
-        fmt.Println("something is wrong")
-    }*/
-
-    // set routes to specific methods
 	server.GET("/series/live", series)
 	server.GET("/players/live", players)
 	server.GET("/teams/live", teams)
 
-	server.Run() // listen and serve on 0.0.0.0:8080
+	server.Run()
 }
 
 func series(c *gin.Context) {
-    // download
-    //statusCode, jsonData := getBody(ABIOS_URL + "/series")
     _, liveSeries := getLiveSeries()
 
     c.IndentedJSON(200, liveSeries)
 }
 
 func players(c *gin.Context) {
-    // download
-    //statusCode, jsonData := getBody(ABIOS_URL + "/series")
     _, data := getLiveSeries()
 
-    // iterate over teams and return
     players := getNestedKeyFromArray(data, "rosters", "players")
 
     c.IndentedJSON(200, players)
 }
 
 func teams(c *gin.Context) {
-    // download
     _, data := getLiveSeries()
 
-    // iterate over teams and return
     teams := getNestedKeyFromArray(data, "rosters", "teams")
 
     c.IndentedJSON(200, teams)
 }
 
-func getAccessToken(clientToken string, clientId string) (string, error) {
+func getAccessToken(clientToken string, clientId string) (string) {
 
 	url := ABIOS_URL +  "/oauth/access_token"
 
@@ -87,40 +69,39 @@ func getAccessToken(clientToken string, clientId string) (string, error) {
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
-    value, err := jsonparser.GetString(body, "access_token")
-    if err != nil {
-        fmt.Println("No access token found", string(body), res.StatusCode)
+    var at map[string]string
+    json.Unmarshal(body, &at)
+
+    if res.StatusCode != 200 {
+        fmt.Println("No token found", at)
         os.Exit(1)
     }
 
-    fmt.Println(value)
-    return value, err
+    return at["access_token"]
 }
 
 
 func getBody(url string) (int, []byte) {
+    limiter := time.Tick(500 * time.Millisecond)
+    <-limiter
+
     req, _ := http.NewRequest("GET", url + "?access_token=" + atoken, strings.NewReader(""))
 
 	res, _ := http.DefaultClient.Do(req)
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-
     return res.StatusCode, body
 }
 
 func getLiveSeries() (error, []map[string]interface{}) {
-    //var returnValue []byte
     var jn map[string]interface{}
     var returnValue []map[string]interface{}
     var err error
 
-    //_, jsonData := getBody(ABIOS_URL + "/series")
-
-    jsonData, err := ioutil.ReadFile("src/main/series.txt")
+    _, jsonData := getBody(ABIOS_URL + "/series")
     json.Unmarshal(jsonData, &jn)
 
-    // iterate over data
     series := getKeyAndReturnArray(jn, "data")
 
     for _, serie := range series {
@@ -155,14 +136,5 @@ func getNestedKeyFromArray(array []map[string]interface{}, key1 string, key2 str
     }
 
     return returnValue
-
 }
 
-/*func jsonPrettyPrint(in []byte) string {
-    var out bytes.Buffer
-    err := json.Indent(&out, in, "", "\t")
-    if err != nil {
-        return string(in)
-    }
-    return out.String()
-}*/
